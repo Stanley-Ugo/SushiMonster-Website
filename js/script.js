106557cc922407447cd467ecf57b4b5709dde87c
Stanley-Ugo <iamKosmicblast@gmail.com>
2020-10-15 10:47:13 +0100

    selecting the html & body tags with jquery

--- js/script.js
+++ js/script.js
@@ -23,7 +23,12 @@ $(document).ready(function(){
     //Smooth scroll
     $('.nav-item a').click(function(){
         link.preventDefault();
+
         let target = $(this).attr('href');
+
+        $('html, body').animate({
+            
+        })
     })
 
 
